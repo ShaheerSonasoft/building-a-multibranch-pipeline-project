d7f44bd38a5d16c924f982b43176ea3bfc0c28c7
Shaheer Akram <shaheerakram@icloud.com>
2019-11-22 15:38:34 +0500

    sales tag testing

--- JenkinsScripts/start.bat
+++ JenkinsScripts/start.bat
@@ -4,5 +4,6 @@ call cd ..
 ::call npm install forever -g
 ::call forever stopall
 ::call forever start --minUptime 1000 --spinSleepTime 1000 ./bin/www
-call npm start
+::call npm start
+call echo started
 exit
